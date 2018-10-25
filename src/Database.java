32ca78d0f38422db9d227553451b3323a0b522f7
DemianDrake <damian.ibarra@ug.uchile.cl>
2018-10-24 21:37:18 -0300

    modifications to database (incorrect, need to be fixed) and improvements to nodo class (needs to complete str->nodo static function

--- src/Database.java
+++ src/Database.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.io.*;
+import java.nio.file.*;
 
 public class Database {
 	private String path;
@@ -21,7 +22,11 @@ public class Database {
 			}
 			break;
 		case "INSERT":
-			r = insert(query_args);
+			if(query_args[1].equals("INTO") && query_args[3].equals("VALUES")) {
+				r = insert(query_args);
+			} else {
+				throw new IOException("Invalid query");
+			}
 			break;
 		case "ORDER":
 			if(query_args[1].equals("BY")) {
@@ -36,16 +41,39 @@ public class Database {
 		return r;
 	}
 	
+	/** Creates a file with the name of the table, containing as first line the column names
+	 * (There's no column type for simplification)
+	 * 
+	 * @param s	"CREATE TABLE tablename column1 column2 column3 ..."
+	 * 			s[0]	s[1]	s[2]	s[3]	s[4]	s[5]
+	 * @return
+	 */
 	private String createTable(String[] s) {
-		//TODO Create file with table name
+		String filename = path.concat(s[2]);
+		try (PrintWriter p = new PrintWriter(new FileOutputStream(filename))) {
+			for(int i = 3; i < s.length; i++) {
+				p.print(s[i] + " ");
+			}
+			p.print('\n');
+			p.close();
+			// I suppose the output stream is also closed with that?
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		return "";
 	}
 	
 	private String insert(String[] s) {
-		/* TODO
-		 * 	Open File
-		 * 	Insert line
-		 */
+		String filename = path.concat(s[2]);
+		try (PrintWriter p = new PrintWriter(new FileOutputStream(filename))) {
+			for(int i = 4; i < s.length; i++) {
+				// I hope this doesn't overwrite what's in the file...
+				p.print(s[i] + " ");
+			}
+			p.print('\n');
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		return "";
 	}
 	
--- src/nodo.java
+++ src/nodo.java
@@ -1,18 +1,99 @@
 package src;
 
 import java.util.*;
+import java.util.Map.Entry;
 
+// I deeply dislike starting a class name with lower case
+// but I just do as the sheet says
 public class nodo {
-	/*
-	 * The line of database table
-	 * key -> string
-	 * value -> anything
-	 */
-	private Map<String, Object> line;
+	private Map<String, Value> dict;
+	
+	private class Value {
+		private Object val;
+		private String type;
+		
+		public Value(String v) {
+			val = v;
+			type = "String";
+		}
+		
+		public Value(int v) {
+			val = v;
+			type = "Integer";
+		}
+		
+		public Object get() {
+			return val;
+		}
+		
+		public String getType() {
+			return type;
+		}
+	}
 	
 	public nodo() {
-		line = new HashMap<String, Object>();
+		dict = new HashMap<String, Value>();
 	}
+	
+	public void put(String key, int value) {
+		dict.put(key, new Value(value));
+	}
+	
+	public void put(String key, String value) {
+		dict.put(key, new Value(value));
+	}
+	
+	public String getAsString(String key) throws Exception {
+		Value value = dict.get(key);
+		if (value.getType().equals("String")) {
+			return value.get().toString();
+		} else {
+			throw new Exception("Value is not a String");
+		}
+	}
+	
+	public int getAsInt(String key) throws Exception {
+		Value value = dict.get(key);
+		if (value.getType().equals("Integer")) {
+			return (Integer) value.get();
+		} else {
+			throw new Exception("Value is not an Integer");
+		}
+	}
+	
+	/** Produces a string from a node
+	 * 
+	 * @return	a string in format: "(key1=[type,value1];key2=[type,value2];...)\n"
+	 */
+	public String parseToSring() {
+		String r = "(";
+		Value v;
+		for (Entry<String, Value> entry : dict.entrySet()) {
+			r += entry.getKey() + "=[";
+			v = entry.getValue();
+			r += v.getType() + ",";
+			if (v.getType().equals("String")) {
+				r += (String) v.get();
+			} else if (v.getType().equals("Integer")) {
+				r += Integer.toString((int)v.get());
+			}
+			r += "];";
+		}
+		r += "\n";
+		return r;
+	}
+	
+	/** Produces a node from a string in a given format
+	 * 
+	 * @param str	(key1=[type,value1];key2=[type,value2];...)
+	 * @return		A node from class nodo with the given data
+	 */
+	public static nodo strToNode(String str) {
+		nodo r = new nodo();
+		// TODO
+		return r;
+	}
+	
 	
 	/**
 	 * Constructs a nodo based on a string in nodo's line format.
@@ -21,26 +102,23 @@ public class nodo {
 	 *   			TableName("column1":value1;"column2":value2;...)
 	 */
 	public nodo(String str) {
+		this();
 		readKeyValue(str.substring(str.indexOf('('), str.length() - 1));
 	}
 	
 	private void readKeyValue(String str) {
 		int i = str.indexOf(':'), j = str.indexOf(';');
 		String key = str.substring(0, i);
-		String stringValue = str.substring(i + 1, j);
+		String strValue = str.substring(i + 1, j);
 		try {
-			int intValue = Integer.parseInt(stringValue);
-			line.put(key, intValue);
+			int intValue = Integer.parseInt(strValue);
+			dict.put(key, new Value(intValue));
 		} catch(NumberFormatException e) {
-			line.put(key, stringValue);
+			dict.put(key, new Value(strValue));
 		}
 		String rest = str.substring(j + 1);
 		if(!rest.equals("")) {
 			readKeyValue(rest);
 		}
-	}
-	
-	public Object get(String key) {
-		return line.get(key);
 	}
 }
